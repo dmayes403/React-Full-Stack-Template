47d4b2729947df2b8531a9b6f27eafd6b4d24a72
dmayes403 <davidrmayes@gmail.com>
2017-10-26 22:55:19 -0600

    updating template

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
+// ^^ the two lines above allow us to make API calls from the console using the attached cookies for authentication.
 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    // ^^ this is the glue to the redux store
+    document.querySelector('#root')
+    // ^^ this takes the App component, and renders it to the div with an id of "root" inside
+    // the public/index.html file
+);
