3d1755da33b7b299bfe0e3093d8050f135500ffb
dmayes403 <davidrmayes@gmail.com>
2017-10-26 22:35:49 -0600

    adding to template

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const epxress = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys')
+
+// *** wire this up for authentication (services) *** require('./services/passport');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        // ^^ this is equal to: 30 days * 24 hours * 60 minutes * 60 seconds * 1000 miliseconds.
+        keys: [keys.cookieKey]
+        // ^^ this is used to encrypt. These two properties are required for cookies. Multiple keys are
+        // allowed for multiple levels of security
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+if (process.env.NODE_ENV = 'productions') { // ****
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
